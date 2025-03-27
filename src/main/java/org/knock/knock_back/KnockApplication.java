cf19a2c0b5a0cbfba299fd59f627327d69342bc8
나큰솔 <knsol1992@e-hcg.com>
2025-03-27 16:04:26 +0900

    port 관련 변경

--- src/main/java/org/knock/knock_back/KnockApplication.java
+++ src/main/java/org/knock/knock_back/KnockApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 @EnableScheduling
 @EnableAsync(proxyTargetClass = true)
 @SpringBootApplication
@@ -12,8 +16,15 @@ public class KnockApplication {
 
     public static void main(String[] args) {
 
-        System.out.println("Knock Application");
-        SpringApplication.run(KnockApplication.class, args);
+        SpringApplication app = new SpringApplication(KnockApplication.class);
 
+        // Heroku 환경 변수 PORT 읽어서 설정
+        String port = System.getenv("PORT");
+        Map<String, Object> props = new HashMap<>();
+        // 로컬 fallback
+        props.put("server.port", Objects.requireNonNullElse(port, 8080));
+
+        app.setDefaultProperties(props);
+        app.run(args);
     }
 }
