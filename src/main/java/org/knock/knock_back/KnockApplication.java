64611e546b6811b9fcd669775cc63ae285937ef7
나큰솔 <knsol1992@e-hcg.com>
2025-04-04 10:56:57 +0900

    Runtime ShutdownHook 추가

--- src/main/java/org/knock/knock_back/KnockApplication.java
+++ src/main/java/org/knock/knock_back/KnockApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -17,6 +20,21 @@ public class KnockApplication {
     public static void main(String[] args) {
 
         SpringApplication app = new SpringApplication(KnockApplication.class);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            final ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+            final ThreadInfo[] threadInfos = threadMXBean.getThreadInfo(threadMXBean.getAllThreadIds(), 100);
+            for (ThreadInfo threadInfo : threadInfos) {
+                System.out.println(threadInfo.getThreadName());
+                final Thread.State state = threadInfo.getThreadState();
+                System.out.println("   java.lang.Thread.State: " + state);
+                final StackTraceElement[] stackTraceElements = threadInfo.getStackTrace();
+                for (final StackTraceElement stackTraceElement : stackTraceElements) {
+                    System.out.println("        at " + stackTraceElement);
+                }
+                System.out.println("\n");
+            }
+        }));
 
         // Heroku 환경 변수 PORT 읽어서 설정
         String port = System.getenv("PORT");
